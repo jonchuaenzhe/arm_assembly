80c5b5a6603a7e41d3a62660f9460050eea6e350
Jonathan Chua <chuaenzhe@gmail.com>
2021-01-13 19:40:24 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,3 @@
-/**
- ******************************************************************************
- * @project        : EE2028 Assignment 1 Program Template
- * @file           : main.c
- * @author         : Gu Jing, ECE, NUS
- * @brief          : Main program body
- ******************************************************************************
- *
- * @attention
- *
- * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
- * All rights reserved.</center></h2>
- *
- * This software component is licensed by ST under BSD 3-Clause license,
- * the "License"; You may not use this file except in compliance with the
- * License. You may obtain a copy of the License at:
- *                        opensource.org/licenses/BSD-3-Clause
- *
- ******************************************************************************
- */
-
-// !IMPORTANT: this file should not be modified unless it is required by the assessors during assessment
-// this project is designed to use semihosting to enable printf() in STM32CubeIDE, refer to wiki.nus
-
 #include "stdio.h"
 
 #define F 5
